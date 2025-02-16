12beef5c621e2d17325ab3d2df2f8d989825a222
Tim OConnor <123276649+tim29oco@users.noreply.github.com>
2025-02-15 21:47:15 -0500

    main.py

--- Days_20_to_29/Day 25 CSV Data/Guess All 50 States/main.py
+++ Days_20_to_29/Day 25 CSV Data/Guess All 50 States/main.py
@@ -7,19 +7,19 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-
 df = pandas.read_csv("50_states.csv")
 all_states = df.state.to_list()
 
 guessed_states = []
 
+print("Type exit to leave")
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Correct", prompt="What's another state name?")
     answer_state = answer_state.title()
     if answer_state == 'Exit':
         missing_states = [state for state in all_states if state not in guessed_states]
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("missed_states")
+        new_data = pandas.DataFrame(missing_states, columns=["Missing States"])
+        new_data.to_csv("missed_states.csv", index=False)
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -29,6 +29,3 @@ while len(guessed_states) < 50:
         state_data = df[df.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)
-
-missed_states = {"Missing States": missing_states}
-missed_states.to_csv("missed_states")
